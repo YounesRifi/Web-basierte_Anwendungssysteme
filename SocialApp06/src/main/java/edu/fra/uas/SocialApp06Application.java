b6b1d40411388474cd1bcfc555442f118c0afff3
YounesRifi <104152114+YounesRifi@users.noreply.github.com>
2023-02-26 22:10:17 +0100

    Update SocialApp06Application.java

--- SocialApp06/src/main/java/edu/fra/uas/SocialApp06Application.java
+++ SocialApp06/src/main/java/edu/fra/uas/SocialApp06Application.java
@@ -1,6 +1,6 @@
 package edu.fra.uas;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
